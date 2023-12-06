d65b64c7e0f1e975000e7c4bf1fb79d2fc9465d5
Raghavendra K R <raghavendra@sodaru.com>
2023-12-06 20:59:48 +0530

    websocket OnMessage is implemented

--- serverless/functions/wsOnMessage.ts
+++ serverless/functions/wsOnMessage.ts
@@ -1,8 +1,57 @@
-import { APIGatewayProxyWebsocketHandlerV2 } from "aws-lambda";
+import { RouteBuilder, Message } from "somod-websocket-extension";
+import { MessageInput } from "../../lib/types";
+import { threadCache } from "../../lib/threadCache";
+import { putMessage } from "../../lib/message";
+import { v1 as v1uuid } from "uuid";
 
-const WebsocketOnMessage: APIGatewayProxyWebsocketHandlerV2 = event => {
-  // eslint-disable-next-line no-console
-  console.log(event);
-};
+const builder = new RouteBuilder();
 
-export default WebsocketOnMessage;
+builder.add(
+  "$default",
+  async (message: Message<MessageInput & { wsMsgId: string }>) => {
+    const userId = message.body.from;
+
+    const thread = await threadCache.get(message.body.threadId);
+
+    if (thread === undefined) {
+      return {
+        statusCode: 400,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ message: "Invalid threadId : does not exist" })
+      };
+    }
+
+    if (!thread.participants.includes(userId)) {
+      return {
+        statusCode: 400,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          message: `Invalid threadId : from '${userId}' is not a participant in thread '${thread.id}'`
+        })
+      };
+    }
+
+    const messageResult = await putMessage(
+      process.env.MESSAGE_BOX_TABLE_NAME + "",
+      userId,
+      {
+        ...message.body,
+        id: v1uuid().split("-").join(""),
+        sentAt: Date.now()
+      }
+    );
+
+    return {
+      statusCode: 200,
+      headers: { "Content-Type": "application-json" },
+      body: JSON.stringify({
+        wsMsgId: message.body.wsMsgId,
+        id: messageResult.id,
+        seqNo: messageResult.seqNo,
+        sentAt: messageResult.sentAt
+      })
+    };
+  }
+);
+
+export default builder.getHandler();
