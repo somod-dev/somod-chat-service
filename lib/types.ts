67d56dbf7d6958bb0a27598d8315c64939f2079e
Lokesh <>
2025-04-11 18:03:44 +0530

    audio video call type and actions added

--- lib/types.ts
+++ lib/types.ts
@@ -13,7 +13,7 @@ export type Thread = {
 
 export type MessageInput = {
   threadId: string;
-  type: "text" | "image" | "control";
+  type: "text" | "image" | "control" | "call";
   action:
     | "new"
     | "edit"
@@ -21,7 +21,11 @@ export type MessageInput = {
     | "sessionStart"
     | "sessionExtend"
     | "sessionEnd"
-    | "sessionRequirementChange";
+    | "sessionRequirementChange"
+    | "initiated"
+    | "connected"
+    | "disconnected"
+    | "ended";
   message: string;
   sessionToken?: string;
 };
@@ -49,6 +53,10 @@ export const typeToAllowedActionsMap = {
     "sessionStart",
     "sessionExtend",
     "sessionEnd",
-    "sessionRequirementChange"
+    "sessionRequirementChange",
+    "initiated",
+    "connected",
+    "disconnected",
+    "ended"
   ]
 };
